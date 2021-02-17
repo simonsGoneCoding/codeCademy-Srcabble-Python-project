7af72ab3b3df4a320d2582ccfdeee5b2ac412b3e
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-17 15:17:03 +0000

    Complete all buletpoints.

---
+++ app.py
@@ -0,0 +1,55 @@
+# In this project, you will process some data from a group of friends playing scrabble. You will use dictionaries to organize players, words, and points.
+
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+
+###### Built point dictionary
+letter_to_points = {key:value for key, value in zip(letters, points)}
+
+# letter_to_points[" "] = 0 
+letter_to_points.update({" ": 0})
+# print(letter_to_points)
+
+
+###### Score a word
+def score_word(word):
+  total_points = 0
+  upper_word = word.upper()
+  
+  for letter in upper_word:
+    total_points += letter_to_points.get(letter, 0)
+
+  return total_points
+
+
+
+score_for_first_word = score_word("brownie")
+print(score_for_first_word)
+
+
+
+######  Score a Game
+
+player_to_words = {
+    "player1": ["blue", "Tennis", "exit"],
+    "wordNerd" : ["earth", "eyes", "Machine"], 
+    "Lexi Con": ["eraser", "belly", "husky"],
+    "Prof Reader": ["zap", "coma", "period"]
+}
+
+
+player_to_points = {}
+
+for player, words in player_to_words.items():
+    player_points = 0
+    for word in words:
+        player_points += score_word(word)
+    player_to_points[player] = player_points
+
+print(player_to_points)
+
+
+
+
